a67267074b5201dd37ac2007638962ae557f0618
nickssr <nickssr@users.noreply.github.com>
2026-04-29 17:36:29 +0800

    Astro v1.0: All pages + blog structure

---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+export default {
+  content: ['./src/**/*.{astro,html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1a1a1a',
+        accent: '#166534'
+      }
+    }
+  }
+};
